000d88dfea211c2f799b9e8224fe6b3c51dec888
GuilhermeLouback <guilherme_lou@hotmail.com>
2023-10-09 15:05:33 -0300

    Finalizando formulario

--- src/views/login.php
+++ src/views/login.php
@@ -24,26 +24,26 @@
                 <span class="font-weight-bold">U</span>
                 <span class="font-weight-light">ltra</span>
                 <span class="font-weight-bold">S</span>
-                <span class="font-weight-light">afe</span>               
+                <span class="font-weight-light">afe</span>
             </div>
             <div class="card-body">
                 <?php include(TEMPLATE_PATH . '/messages.php') ?>
                 <div class="form-group">
                     <label for="email">E-mail</label>
                     <input type="email" id="email" name="email"
-                        class="form-control <?= $errors ['email'] ? 'is-invalid' : '' ?>" value="<?= $email ?>"
+                        class="form-control <?= $errors['email'] ? 'is-invalid' : '' ?>" value="<?= $email ?>"
                         placeholder="Informe o e-mail" autofocus>
                     <div class="invalid-feedback">
-                        <?= $errors ['email'] ?>
+                        <?= $errors['email'] ?>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="password">Senha</label>
                     <input type="password" id="password" name="password"
-                        class="form-control <?= $errors ['password'] ? 'is-invalid' : '' ?>"
+                        class="form-control <?= $errors['password'] ? 'is-invalid' : '' ?>"
                         placeholder="Informe a senha">
                     <div class="invalid-feedback">
-                        <?= $errors ['password'] ?>
+                        <?= $errors['password'] ?>
                     </div>
                 </div>
             </div>
